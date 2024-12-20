8b92b79bd90194d57cf9586e5402d083b67848ab
KailashSatkuri-warangal <satkurikailash@gmail.com>
2024-12-20 23:03:45 +0530

    Initial commit

---
+++ data/Experience.js
@@ -0,0 +1,65 @@
+export const ExperienceList = [
+    {
+        company: "Monosage",
+        role: "Software Developer Intern",
+        from: "August 2024",
+        to: "Present",
+        color: "linear-gradient(351deg, rgba(255,170,0,1) 1%, rgba(236,147,39,1) 67%)",
+        logo: '/work/monosage.jpg',
+        activities: 'Developed two WordPress sites, boosting client retention and increasing inquiries and hosted them.'
+    }
+   /* {
+        company: "none",
+        role: "Software Developer Intern",
+        from: "July 2025",
+        to: "Present",
+        color: "linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%)",
+        logo: '',
+        activities: 'De'
+    },
+    {
+        company: "",
+        role: "Technology Summer Analyst",
+        from: "May 2025",
+        to: "July 202",
+        color: "linear-gradient(351deg, rgba(2,0,36,1) 0%, rgba(19,172,204,1) 0%, rgba(0,212,255,1) 100%)",
+        logo: '',
+        activities: 'D'
+    },
+    {
+        company: "",
+        role: "Frontend Developer Intern",
+        from: "Novemeber 2025",
+        to: "March 202",
+        color: "linear-gradient(351deg, rgba(2,0,36,1) 0%, rgba(204,19,19,1) 0%, rgba(45,203,81,1) 1%, rgba(255,0,173,1) 100%)",
+        logo: '',
+        activities: 'h'
+    },
+    {
+        company: "",
+        role: "Software Developer Intern",
+        from: "January 2025",
+        to: "May 202",
+        color: "linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%)",
+        logo: '',
+        activities: 'Aunsactions'
+    },
+    {
+        company: "Google Summer of Code 2026",
+        role: "Contributor",
+        from: "May 2025",
+        to: "November 202",
+        color: "linear-gradient(-20deg, rgb(186 142 7) 0%, rgb(253 151 33) 100%)",
+        logo: '',
+        activities: 'W'
+    },
+    {
+        company: "",
+        role: "Full Stack Developer",
+        from: "August 2025",
+        to: "November 2025",
+        color: "linear-gradient(-20deg, rgb(186 7 125) 0%, rgb(253 33 63) 100%)",
+        logo: '',
+        activities: 'frontend.'
+    }*/
+]
---
+++ data/Projects.js
@@ -0,0 +1,65 @@
+export const ProjectsList = [{
+        name: 'IoT anomaly detection',
+        description: 'Here are the IoT anomaly detection projects I have implemented that deals with the identification of cases that may indicate security breaches or hacks in IoT systems. The projects employ decision tree, random forest and linear regression to search for irregularities underlying the data: to detect any kind of intruding or malicious attempts. Through these models, the machine learning analysis of the accumulated IoT information and data helps the system to identify and even prevent unusual patterns so as to protect the assets of IoT devices and networks.',
+        image: '/Projects/iot.png',
+        github: 'https://github.com/KailashSatkuri-warangal/AIML-project-IoT-Device-Anomaly-Detection',
+        hosted: false,
+        host_link: 'https://github.com/KailashSatkuri-warangal/AIML-project-IoT-Device-Anomaly-Detection'
+    },
+    {
+        name: 'LearnVidya LMS portel',
+        description: 'An Intership program where LearnVidya is a Learning Management System (LMS) portal developed using WordPress, designed to facilitate online learning and course management. The platform allows educators to create, manage, and deliver online courses to students, providing an interactive and user-friendly environment. It includes features like course enrollment, quizzes, assignments, discussion forums, and certificates upon course completion. LearnVidya is built with custom WordPress themes and plugins to ensure scalability, flexibility, and an intuitive user experience. The portal also supports various multimedia content formats, such as videos, PDFs, and interactive lessons, to enhance the learning process.',
+        image: '/Projects/lms.png',
+        github: null,
+        hosted: true,
+        host_link: 'https://www.onlinelearnvidya.great-site.net/?i=1'
+    },
+    {
+        name: 'Web Travel Website',
+        description: 'An Intership program with self learned and integrated with backend php and sql website',
+        image: '/Projects/travel.png',
+        github: 'https://github.com/monosage-warangal/kailash-localhost',
+        hosted: true,
+        host_link: 'https://webtravel.great-site.net/index.php'
+    },
+    {
+        name: 'Test to image Generative AI Project',
+        description: 'Implemented a machine learning model for generating images from text descriptions using GANs and diffusion models. This project demonstrates the power of combining NLP and image generation for creative applications.',
+        image: '/Projects/toi.png',
+        github: 'https://github.com/KailashSatkuri-warangal/Text-to-image-Generation',
+        hosted: false,
+        host_link: 'https://github.com/KailashSatkuri-warangal/Text-to-image-Generation'
+    }/*,
+    {
+        name: 'Website',
+        description: '',
+        image: '/Projects/.png',
+        github: '',
+        hosted: true,
+        host_link: ''
+    },
+    {
+        name: 'y',
+         description: '',
+         image: '/Projects/.png',
+        github: null,
+        hosted: true,
+        host_link: ''
+    },
+    {
+        name: 'Qe',
+         description: '',
+         image: '/Projects/.png',
+        github: '',
+        hosted: true,
+        host_link: ''
+    },
+    {
+        name: 'Cart',
+         description: '',
+         image: '/Projects/.png',
+        github: '',
+        hosted: true,
+        host_link: ''
+    }*/
+]
